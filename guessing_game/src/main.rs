c0c877e2b7adddad93b54f4915017374a5006a8a
Pu Cute <pucute@MacBook-Pro-cua-Pu.local>
2022-05-06 22:27:29 +0700

    handle invalid variable and loop in range

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,18 +8,31 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("The secret number is: {}", secret_number);
 
-    loop {
+    for n in 1..5 {
         let mut guess = String::new(); //mut - mutable
-        println!("Please input your guess: ");
+        println!("Please input your guess turn {}: ", n);
         io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let guess :u32 = guess.trim().parse().expect("Please type a number"); //shadow the previous value of variable
+        let guess :u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        }; //shadow the previous value of variable
         println!("You guessed in {}: {}", today, guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big"),
+            Ordering::Less => {
+                println!("Too small");
+                if n == 4 {
+                    println!("=>>>>>Looser");
+                }
+            },
+            Ordering::Greater => {
+                println!("Too big");
+                if n == 4 {
+                    println!("=>>>>>Looser");
+                }
+            },
             Ordering::Equal => {
-                println!("You win");
+                println!("You win!!!!!!");
                 break;
             }
         }
