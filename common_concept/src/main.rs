f58e0df5c08c3d4d392c2947a21a2f983e9beb3a
Pu Cute <pucute@MacBook-Pro-cua-Pu.local>
2022-05-07 17:45:26 +0700

    data types in rust

--- common_concept/src/main.rs
+++ common_concept/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     const PI: f64  = 3.14;
     println!("Hello, world!");
@@ -19,5 +21,21 @@ fn main() {
     let spaces = "dddd";
     // spaces = spaces.len(); // error because the shadow only change value cannot change varibale's type ( form string to int)
 
-    println!("{}", spaces)
+    println!("{}", spaces);
+
+    // let f: bool = false;
+    // let t = true;
+    let cat = "😻";
+    println!("{}", cat);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    println!("{}", tup.0);
+    let arr: [f32; 5]= [1.1, 2.2, 3.3, 4.4, 5.5];
+    println!("{}", arr[3]);
+
+    let mut index = String::new();
+    io::stdin().read_line(&mut index).expect("Failed to read line");
+    let index: usize = index.trim().parse().expect("Index entered was not a number");
+    let element = arr[index];
+    println!("{} {}", index, element)
 }
