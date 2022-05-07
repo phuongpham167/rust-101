2534bdcc06a0703df3c32131a029f522016b3701
Pu Cute <pucute@MacBook-Pro-cua-Pu.local>
2022-05-07 15:42:42 +0700

    delete target debug folder

--- guessing_game/target/debug/deps/cfg_if-00e0456f358f408c.d
+++
@@ -1,7 +0,0 @@
-/Users/pucute/CODING/CANHAN/RUST_101/guessing_game/target/debug/deps/cfg_if-00e0456f358f408c.rmeta: /Users/pucute/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/pucute/CODING/CANHAN/RUST_101/guessing_game/target/debug/deps/libcfg_if-00e0456f358f408c.rlib: /Users/pucute/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/pucute/CODING/CANHAN/RUST_101/guessing_game/target/debug/deps/cfg_if-00e0456f358f408c.d: /Users/pucute/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/pucute/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/pucute/CODING/CANHAN/RUST_101/guessing_game/target/debug/guessing_game: /Users/pucute/CODING/CANHAN/RUST_101/guessing_game/src/main.rs
