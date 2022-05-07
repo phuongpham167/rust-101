f2e7ba54b4358bf6323c2d50e0cd65bba818b4c5
Pu Cute <pucute@MacBook-Pro-cua-Pu.local>
2022-05-07 15:35:53 +0700

    contants and data types

--- common_concept/src/main.rs
+++ common_concept/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
+    const PI: f64  = 3.14;
     println!("Hello, world!");
     let x = 5; // by default is immutable
     let mut y = 6; //add mut to define y with mutable
-    println!("before change y: x + y = {} + {} = {}", x, y, (x + y));
+    println!("before change y: (x + y)*pi = ({} + {})*pi = {}", x, y, (f64::from(x + y)* PI)); //using f64::from(a) to change a => f64 data type
     y = 5;
-    println!("after change y: x + y = {} + {} = {}", x, y, (x + y));
+    println!("after change y: (x + y)*pi = ({} + {})*pi = {}", x, y, (f64::from(x + y)* PI));
 }
